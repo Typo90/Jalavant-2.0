e58b1872bbb0fbd8a9a05677b1b77115919dce04
Yucheng Peng <yp284@cornell.edu>
2022-11-28 20:12:20 -0500

    Create the empty Android program

---
+++ Android-Programs/app/src/main/java/typo/com/jalavant_2_0/MainActivity.java
@@ -0,0 +1,14 @@
+package typo.com.jalavant_2_0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
